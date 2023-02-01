5d8b76a57cade30500c6cc895493bd666fca43ac
Алексей Воевода <avoevoda433@gmail.com>
2023-02-01 14:33:12 +0300

    Add files via upload

---
+++ Lab_7/Lab7ex1/Lab7ex1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Создаем новый контур
+            GraphicsPath myPath = new GraphicsPath();   
+            //Добавляем элипс в текущий контур
+            myPath.AddEllipse(0, 0, this.Width, this.Height);
+            //Создаем новую часть графической формы с указанным контуром в виде элипса
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Lab_7/Lab7ex2/Lab7ex2/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7ex2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            this.BackColor = Color.Green;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            using (GraphicsPath myPath = new GraphicsPath())
+            {
+                myPath.AddLines(new[] {new Point(0, Height/2),
+                                       new Point(Width/2, 0),
+                                       new Point(Width, Height/2),
+                                       new Point(Width/2, Height)});
+                Region = new Region(myPath);
+            }
+        }
+    }
+}
